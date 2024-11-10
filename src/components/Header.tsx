169651bf095ae1636a2e81bb41b899464d94baa1
Xphi310302 <ngxuanphi010402@gmail.com>
2024-11-10 16:46:16 +0700

    ref: add images src and adjust header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,28 +6,33 @@ const Header: React.FC = () => {
     <header className="header">
       <div className="header-top">
         <div className="logo">
-          <img src="https://app-thuduc.tphcm.gov.vn/image/layout_set_logo?img_id=28701&t=1730431840092" alt="UBND Thành Phố Thủ Đức" /> {/* Add your logo image */}
+          <img src="src/assets/images/layout_set_logo.png" alt="UBND Thành Phố Thủ Đức" /> {/* Add your logo image */}
           {/* <span>UBND THÀNH PHỐ THỦ ĐỨC</span> */}
         </div>
-        <div className="contact-info">
-          <span>📞 (028) 3740 0509</span>
-          <span>✉️ tpthuduc@tphcm.gov.vn</span>
-        </div>
-        <div className="auth-buttons">
-          <button className="create-account">Tạo tài khoản</button>
-          <button className="login">Đăng nhập</button>
+        <div className='top-right-content'>
+          <div className="contact-info">
+            <span>📞 (028) 3740 0509</span>
+            <span>✉️ tpthuduc@tphcm.gov.vn</span>
+          </div>
+          <div className="auth-buttons">
+            <button className="create-account">Tạo tài khoản</button>
+            <button className="login">Đăng nhập</button>
+          </div>
         </div>
       </div>
 
       <div className="header-main">
         <nav>
           <ul className="menu">
-            <li><button>TRANG CHỦ</button></li>
+            <li className='homepage'>
+              <button >
+                <img src="src/assets/images/icHome-hv.png" alt="Home"/>
+              </button>
+            </li>
             <li><button>THỦ TỤC HÀNH CHÍNH</button></li>
             <li><button>TRA CỨU KẾT QUẢ HỒ SƠ</button></li>
             <li><button>THỐNG KÊ</button></li>
             <li><button>ĐĂNG KÝ THI ĐUA GIA ĐÌNH VĂN HÓA</button></li>
-        
           </ul>
         </nav>
       </div>
