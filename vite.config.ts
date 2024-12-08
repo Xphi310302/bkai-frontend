7f590507d7063cc748818cea527e774f8f109f24
Xphi310302 <ngxuanphi010402@gmail.com>
2024-12-08 17:40:05 +0700

    ref: add CORS 2

--- vite.config.ts
+++ vite.config.ts
@@ -13,10 +13,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Listen on all interfaces
     port: 5173,      // Specify the port if needed
-    cors: {
-      origin: '*', // Allow all origins, adjust as needed
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-      allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    },
   },
 })
