8bfdcdaddec7a73f62fda933e96079ef656ec24d
Xphi310302 <ngxuanphi010402@gmail.com>
2024-12-08 17:41:07 +0700

    ref: roll back CORS

--- src/services/axios-config.ts
+++ src/services/axios-config.ts
@@ -7,22 +7,7 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true',
-    'Access-Control-Allow-Origin': '*', // Allow all origins
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
   }
 });
-
-// Add an interceptor to handle CORS preflight requests
-axiosInstance.interceptors.request.use(
-  config => {
-    // Ensure OPTIONS method is handled for preflight requests
-    if (config.method === 'options') {
-      config.headers['Access-Control-Request-Method'] = config.method.toUpperCase();
-    }
-    return config;
-  },
-  error => Promise.reject(error)
-);
 
 export default axiosInstance;
